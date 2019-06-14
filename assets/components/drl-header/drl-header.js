5ee76388f13e8fc807d61174bfc6a419c31d7b59
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-06-14 17:32:20 +0700

    update drl-header: add menu

--- assets/components/drl-header/drl-header.js
+++ assets/components/drl-header/drl-header.js
@@ -7,12 +7,57 @@
     window.app
         .component('drlHeader', {
             template: require('./drl-header.html'),
-            controller: drl
+            controller: drl,
+            bindings: {
+                menu: '=',
+                menuActiveNow: '='
+            }
         });
 
-    drl.$inject = [];
-    function drl() {
+    drl.$inject = ['$scope'];
+    function drl($scope) {
         let $ctrl = this;
-        $ctrl.$onInit = () => { };
+        const clearActive = () => {
+            $ctrl.menu.sidebar.forEach(menu => {
+                if (menu.hasOwnProperty('active')) {
+                    menu.active = false;
+                }
+                if (menu.hasOwnProperty('menu')) {
+                    menu.menu.forEach(_menu => {
+                        if (_menu.hasOwnProperty('active')) {
+                            _menu.active = false;
+                        }
+                    });
+                }
+            });
+            $ctrl.menu.dropdownC.forEach(menu => {
+                if (menu.hasOwnProperty('active')) {
+                    menu.active = false;
+                }
+            })
+            $ctrl.menuActiveNow = [];
+        };
+
+        $ctrl.$onInit = () => {
+            const checkActive = () => {
+                $ctrl.menuActiveNow = [];
+                $ctrl.menu.dropdownC.forEach(menu => {
+                    if (menu.hasOwnProperty('active')) {
+                        if (menu.active) {
+                            $scope.active(menu, menu.href);
+                        }
+                    }
+                });
+            };
+
+            checkActive();
+        };
+
+        $scope.active = (element, href) => {
+            clearActive();
+            // go to href disini
+            console.log({ href });
+            $ctrl.menuActiveNow.push(element);
+        };
     }
 })();
